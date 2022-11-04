698e5ba643d99ed4e327a0a60ca60fe71a68392a
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-04 16:05:33 +0100

    test

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ // input 
+let nome = parseInt(prompt("Inserisci il tuo nome"));
+
+console.log(nome);
+
+let cognome = parseInt(prompt("Inserisci il tuo cognome"));
+
+console.log(cognome);
+
+// output
+
+document.getElementById("password").innerHTML = "$(nome)$(cognome)21";

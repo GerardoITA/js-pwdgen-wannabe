7a5d4a05ed4daac42ebed3082646476002794aff
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-04 16:28:41 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let cognome = (prompt("Inserisci il tuo cognome"));
 
 console.log(cognome);
 
+let genpassword = nome + cognome + 21;
 // output
 
-document.getElementById("password").innerHTML = "${nome} ${cognome} 21";
+document.getElementById("password").innerHTML = (genpassword);
